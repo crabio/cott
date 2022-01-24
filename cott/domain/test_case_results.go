a75fcaab72c5d37edc01cc01bfe3751d48b9985e
Ivan Shchukin <shchukin@arrival.com>
2022-01-24 20:43:03 +0300

    Fix report saving

--- cott/domain/test_case_results.go
+++ cott/domain/test_case_results.go
@@ -1,20 +1,20 @@
 package domain
 
 type TestCaseResults struct {
-	TestCase *TestCase `json:"test-case"`
-	Score    float32   `json:"score"`
-	Metrics  []*Metric `json:"metrics,omitempty"`
-	Error    *string   `json:"error,omitempty"`
+	TestCase TestCase `json:"test-case"`
+	Score    float32  `json:"score"`
+	Metrics  []Metric `json:"metrics,omitempty"`
+	Error    *string  `json:"error,omitempty"`
 }
 
 func NewTestCaseResults(tc *TestCase) *TestCaseResults {
 	r := new(TestCaseResults)
-	r.TestCase = tc
+	r.TestCase = *tc
 	return r
 }
 
 func (r *TestCaseResults) AddMetric(name string, uofp UnitOfMeasurePrefix, uom UnitOfMeasure, value float64) {
-	r.Metrics = append(r.Metrics, &Metric{
+	r.Metrics = append(r.Metrics, Metric{
 		Name:                name,
 		UnitOfMeasurePrefix: uofp,
 		UnitOfMeasure:       uom,
